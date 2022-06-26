c318826665875074daa2fd27e30ab8d099204fc8
arq_rizal <221810196@stis.ac.id>
2022-06-26 20:12:02 +0700

    change api url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://localhost:8080/auth/'
+const API_URL = 'https://simrt.herokuapp.com/auth/'
 class AuthService {
   login (user) {
     return axios
