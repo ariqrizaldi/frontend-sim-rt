0d050cd35a1a8918f1749e16829c2abc7b802fde
arq_rizal <221810196@stis.ac.id>
2022-06-26 21:48:32 +0700

    ubah z index

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -61,7 +61,7 @@ const logOut = () => {
 <template>
   <nav
     v-show="isNavBarVisible"
-    class="top-0 left-0 right-0 fixed flex bg-white h-14 border-b z-30 w-screen shadow-md
+    class="top-0 left-0 right-0 fixed flex bg-white h-14 border-b z-40 w-screen shadow-md
     transition-position lg:w-auto lg:items-stretch dark:bg-gray-900 dark:border-gray-800"
     :class="[lightBorderStyle, {'ml-60 lg:ml-0':isAsideMobileExpanded}] + [ !props.aside ? 'xl:pl-60' : '']"
   >
