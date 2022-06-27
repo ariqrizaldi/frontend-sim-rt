4e4f6c7f57e79be6b40ddfbede61799bbc8bcb5c
arq_rizal <221810196@stis.ac.id>
2022-06-27 08:42:05 +0700

    ubah hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 // import Admin from '@/layouts/Admin.vue'
 import User from '@/layouts/User.vue'
 import Staff from '@/layouts/Staff.vue'
@@ -524,7 +524,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior (to, from, savedPosition) {
     return savedPosition || { top: 0 }
